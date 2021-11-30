5d583ec9d585ca2b1279e03a3287a2463c059aed
Brandon Fan <brandonfan1256@gmail.com>
2021-11-30 12:37:27 -0500

    update docker build

--- build_docker.sh
+++ build_docker.sh
@@ -1,1 +1,2 @@
 docker build -t us-central1-docker.pkg.dev/mig-platform/data-collection-repos/reddit:$(git rev-parse origin/master) .
+docker push us-central1-docker.pkg.dev/mig-platform/data-collection-repos/reddit:$(git rev-parse origin/master)
